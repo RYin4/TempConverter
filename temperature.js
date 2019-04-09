193ef08a32ceecf7bedf7eefd9dcd9ed5a9696e9
Randy Yin <randyyin28@gmail.com>
2019-04-09 12:25:58 -0700

    Update temperature.js

--- temperature.js
+++ temperature.js
@@ -3,7 +3,7 @@ const input = document.querySelector("#input > input");
 const output = document.querySelector("#output > input");
 
 function inputToCelcius() {
-    if (document.getElementById('temp_c').checked) {
+    if (document.getElementById('temp_f').checked) {
 
         //if the input.value contains any letters = output is false
         let isNum = /^\d+$/.test(input.value);
@@ -11,16 +11,17 @@ function inputToCelcius() {
             console.log("true")
             const fTemp = parseFloat(input.value);
             const cTemp = (fTemp - 32) * (5/9);
-            output.value = cTemp;
+            const n = cTemp.toPrecision(6);
+            output.value = `${fTemp} F = ${n}`
         } else {
-            window.alert("Numbers only!")
+            window.alert("ERROR: Numbers Only!")
             console.log("false")
         }
     }
 }
 
 function inputToFahrenheit() {
-    if (document.getElementById('temp_f').checked) {
+    if (document.getElementById('temp_c').checked) {
 
         //if the input.value contains any letters = output is false
         let isNum = /^\d+$/.test(input.value);
@@ -28,18 +29,29 @@ function inputToFahrenheit() {
             console.log("true")
             const cTemp = parseFloat(input.value);
             const fTemp = (cTemp * (9/5)) + 32;
-            output.value = fTemp;
+            const n = fTemp.toPrecision(6);
+            output.value = `${cTemp} C = ${n} F`;
         } else {
-            window.alert("Numbers only!")
+            window.alert("ERROR: Numbers Only!")
             console.log("false")
         }
     }
 }
 
 
-if (!document.getElementById('temp_c').checked && !document.getElementById('temp_f').checked ) {
-    window.alert("No Temperature Scale Has Been Selected")
+// if (!document.getElementById('temp_c').checked && !document.getElementById('temp_f').checked ) {
+//     window.alert("No Temperature Scale Has Been Selected")
+// }
+
+const i = document.getElementById("input")
+if (input.value === true) {
+    console.log("yes")
+} else {
+    console.log("no")
 }
 
 input.addEventListener('input', inputToCelcius);
 input.addEventListener('input', inputToFahrenheit);
+
+
+
